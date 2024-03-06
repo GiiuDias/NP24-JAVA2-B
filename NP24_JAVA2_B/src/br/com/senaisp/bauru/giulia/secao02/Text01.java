c3dfd80fef057b7df4c624bcfdff60ac034cfd7c
GiiuDias <giuliadias63@gmail.com>
2024-03-05 22:06:37 -0300

    Conteudo da Aula 05-02-2024

---
+++ NP24_JAVA2_B/src/br/com/senaisp/bauru/giulia/secao02/Text01.java
@@ -0,0 +1,19 @@
+package br.com.senaisp.bauru.giulia.secao02;
+
+public class Text01 {
+	/** Comentário de documentação
+	 * 
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	//Comentário de uma linha
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
