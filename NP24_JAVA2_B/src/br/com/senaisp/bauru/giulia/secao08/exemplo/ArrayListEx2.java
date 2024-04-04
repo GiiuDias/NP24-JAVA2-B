d4f06d2550ee255ce586b8160daf0f5903433ed8
giiudias <giuliadias63@gmail.com>
2024-04-03 22:00:17 -0300

    aula 03-04

--- NP24_JAVA2_B/src/br/com/senaisp/bauru/giulia/secao08/exemplo/ArrayListEx2.java
+++ NP24_JAVA2_B/src/br/com/senaisp/bauru/giulia/secao08/exemplo/ArrayListEx2.java
@@ -7,39 +7,47 @@ import java.util.Random;
 
 public class ArrayListEx2 {
     public static void main(String args[]) {
-        ArrayList<Integer> lstInt = new ArrayList<Integer>();
-        Random rnd = new Random();
-        // Adicionando valores aleatórios de 1 a 200 na lista
-        for (int i = 0; i < 100; i++) {
-            lstInt.add(rnd.nextInt(200) + 1);
-        }
-        // listar os valores gerados
-        for (int it : lstInt) {
-            System.out.println(it);
-
-        }
-        // Utilizando o iterator
-        System.out.println("Listando usando iterator");
-        Iterator<Integer> iter = lstInt.iterator();
-        while (iter.hasNext()) { // Removi o ponto e vírgula extra aqui
-            System.out.println(iter.next());
-        }
-        // Removendo os pares
-        Iterator<Integer> it = lstInt.iterator(); // Adicionei um novo iterator para percorrer a lista
-        while (it.hasNext()) { // Usei outro iterator para evitar ConcurrentModificationException
-            int num = it.next(); // Obtendo o próximo elemento
-            if (num % 2 == 0) { // Verificando se é par
-                it.remove(); // Removendo o item par
-            }
-        }
-        // Listando o resultante depois de retirado os valores
-        // Utilizando o listIterator
-        System.out.println("Listando o final");
-        ListIterator<Integer> lsIt = lstInt.listIterator();
-        while (lsIt.hasNext()) { // Corrigi lst para lsIt
-            System.out.println(lsIt.next()); // Adicionei next() para obter o próximo elemento
-        }
-
+    	ArrayList<Integer> lstInt = new ArrayList<Integer>();
+    	Random rnd = new Random();
+    	//Adicionando valores aleatórios de 1 a 200 na lista
+    	for (int i=0;i<100;i++) {
+    		lstInt.add(rnd.nextInt(200)+1);
+    	}
+    	//listar os valores gerados
+    	for(int it : lstInt) {
+    		System.out.println(it);
+    	}
+    	//Utilizando o iterator
+    	System.out.println("Listando usando iterator");
+    	Iterator<Integer> iter = lstInt.iterator();
+    	while(iter.hasNext()) {
+    		System.out.println(iter.next());
+    	}
+    	//removendo os pares => Isso pode dar problemas
+    	//porque eu perco a referencia de indices
+    	/*
+    	for(int it : lstInt) {
+    		if ( it%2==0 ) {
+    			//removendo o item par
+    			lstInt.remove(it);
+    		}
+    	}
+    	*/
+    	//Para resolver o problema, removendo a partir do
+    	//indice do maior para o menor
+    	for (int i=lstInt.size()-1;i>=0;i--) {
+    		if (lstInt.get(i)%2==0) {
+    			lstInt.remove(i);
+    		}
+    	}
+        //Listando o resultante depois de retirado os valores
+    	//Utilizando o listIterator
+    	System.out.println("Listando o final");
+    	ListIterator<Integer> lsIt = lstInt.listIterator();
+    	while (lsIt.hasNext()) {
+    		System.out.println(lsIt.next());
+    	}
+    	System.out.println("Mostrando o último novamente");
+    	System.out.println(lsIt.previous());//volta um indice
     }
-
 }
